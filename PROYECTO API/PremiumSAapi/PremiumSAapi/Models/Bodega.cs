83d2a6e1656cead7ee4ac095658627747916d993
JdanielS7 <jsierrac3@miumg.edu,gt>
2025-11-11 19:56:46 -0600

    modelo bodegas en api

--- PROYECTO API/PremiumSAapi/PremiumSAapi/Models/Bodega.cs
+++ PROYECTO API/PremiumSAapi/PremiumSAapi/Models/Bodega.cs
@@ -13,7 +13,10 @@ namespace PremiumSAapi.Models
         [Column("nombre_bodega")]
         public string NombreBodega { get; set; } = string.Empty;
 
-        [Column("ubicacion")]
-        public string? Ubicacion { get; set; }
+        [Column("ubicacion_bodega")]
+        public string? UbicacionBodega { get; set; }
+
+        [Column("capacidad_bodega")]
+        public double? CapacidadBodega { get; set; }
     }
 }
